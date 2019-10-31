d7257b3e967c2dff1b3d08b355c387cba397fd48
TobyG <toby@wearecrystal.uk>
2019-10-31 13:44:53 +0000

    accidentally do all the logic in 1 go

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config({ path: './.env' });
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.TOKEN);
+
+client.on('ready', () => {
+  console.log('its a crazy night');
+});
+
+client.on('message', msg => {
+  let pinned = [];
+  if (msg.content !== 'its a crazy night') {
+    msg.channel.delete();
+  } else {
+    msg.channel
+      .fetchPinnedMessages()
+      .then(messages => {
+        if (messages.size === 50) {
+          return false;
+        }
+
+        messages.forEach(message => {
+          pinned.push(message.author.username);
+        });
+
+        if (!pinned.includes(msg.author.username)) {
+          msg.pin();
+        }
+      })
+      .catch(console.error);
+  }
+
+  if (msg.member.nickname !== 'its a crazy night') {
+    msg.member.setNickname('its a crazy night');
+  }
+});
